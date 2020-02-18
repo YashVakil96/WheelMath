4dab37b53080d421551dedc524a1809014e3a39d
YashVakil96 <yashvakil96@gmail.com>
2020-02-18 18:00:10 +0530

    Touching The Numbers Work

---
+++ Assets/Resources/Assets/_Script/ButtonTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void TestButton()
+    {
+        Debug.Log(this.name);
+    }
+}
